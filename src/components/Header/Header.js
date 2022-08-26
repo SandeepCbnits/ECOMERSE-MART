e81cdb50425ee81bde4330c2b46a874b7b139395
SandeepCbnits <sandeep.yadav@cbnits.com>
2022-08-26 17:28:07 +0530

    SEARCHING-ARE MAKING

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,44 +4,29 @@ import { NavLink, useNavigate } from "react-router-dom";
 import SideBar from "../SideBar/SideBar";
 import { useSelector } from "react-redux";
 
-const Header = ({ cartItems,products }) => {
+const Header = ({ cartItems, products }) => {
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
   const [searchValue, setSearchValue] = useState();
-  const isLogedIn =useSelector(state=>state.ui.isLogedIn)
+  const isLogedIn = useSelector((state) => state.ui.isLogedIn);
 
- 
   const isOpenHandler = () => {
     setIsOpen(!isOpen);
   };
 
-  const onChangeHadler = async (e) => {
+  const onChangeHadler =  (e) => {
     let key = e.target.value;
     setSearchValue(e.target.value);
     if (key !== "") {
-      let result = await fetch(
-        `http://localhost:9090/products/getByProductId?pid=${key}`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: products.id,
-        }
-      );
-      let response = await result.json();
-      console.log(response);
-      setSearchValue(response)
-    } else {
       setSearchValue(products);
-    }
+    } 
   };
   return (
     <div>
       <div className={style.header}>
         <div className={style.header_cart}>
           <span className={style.logo}>Shoping Mart</span>
-          <input type="number"  onChange={onChangeHadler} />
+          <input type="text" name="search" />
           <span className={style.search_icon}>
             <i class="fa fa-search"></i>
           </span>
@@ -49,9 +34,9 @@ const Header = ({ cartItems,products }) => {
         {/* Condition for login logoout and profile setup */}
         <div className={style.header__signup}>
           <div className={style.header___login}>
-          {!isLogedIn &&  <NavLink to="/" >Login</NavLink>}
-           {isLogedIn && <NavLink to="/profile">Profile </NavLink>}
-          {isLogedIn &&  <NavLink to="/login">Logout</NavLink>}
+            {!isLogedIn && <NavLink to="/">Login</NavLink>}
+            {isLogedIn && <NavLink to="/profile">Profile </NavLink>}
+            {isLogedIn && <NavLink to="/login">Logout</NavLink>}
           </div>
 
           <NavLink to="/cart" className={style.header__cart}>
